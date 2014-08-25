67412747cfd379b9918faa6905980e679dcbd653
Martin Peschke <martin@hackandcraft.com>
2014-08-25 21:56:02 +0200

    updated deplyment

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'CHANGES.md')) as f:
-    CHANGES = f.read()
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
 
 requires = [
     'pyramid',
