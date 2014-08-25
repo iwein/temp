e28d3058500e9df0ed6d48850f481af13639bc12
Martin Peschke <martin@hackandcraft.com>
2014-08-25 21:06:18 +0200

    initials

---
+++ setup.py
@@ -0,0 +1,52 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.md')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    'psycopg2',
+    'static3',
+    'wsgiref',
+    'requests',
+    'paste', 'pastescript', 'pastedeploy'
+    ]
+
+setup(name='scotty',
+      version='0.0',
+      description='scotty',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='scotty',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = scotty:main
+      [console_scripts]
+      initialize_scotty_db = scotty.scripts.initializedb:main
+      """,
+      )
