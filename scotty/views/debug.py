540d04cfccab3cc49f37f4d04e84168a18146acd
Martin Peschke <martin@hackandcraft.com>
2014-08-26 16:37:06 +0200

    simple debug page

--- scotty/views/debug.py
+++ scotty/views/debug.py
@@ -1,4 +1,5 @@
 from pyramid.view import view_config
+import pyramid_mako
 from scotty.views import RootController
 
 
@@ -7,7 +8,7 @@ class DebugController(RootController):
     def debug_home(self):
         return {}
 
-    @view_config(route_name='debug_config', renderer='scotty:templates/debug/config.pt')
+    @view_config(route_name='debug_config', renderer='scotty:templates/debug/config.html')
     def debug_config(self):
         return {}
 
@@ -21,6 +22,10 @@ class DebugController(RootController):
 
 
 def includeme(config):
+    config.add_directive('add_mako_renderer', pyramid_mako.add_mako_renderer)
+    config.add_mako_renderer(".html")
+
+
     config.add_route('debug_home', '')
     config.add_route('debug_config', '/config')
     config.add_route('debug_candidate', '/candidate')
