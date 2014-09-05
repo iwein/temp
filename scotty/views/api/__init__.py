82a55a353db85cfc4c05c05d86ab97a390241508
Martin Peschke <martin@hackandcraft.com>
2014-09-05 17:07:00 +0200

    managed error for signup

--- scotty/views/api/__init__.py
+++ scotty/views/api/__init__.py
@@ -1,4 +1,5 @@
 import json
+from pyramid.httpexceptions import HTTPException
 from pyramid.response import Response
 from scotty.views.api import configuration, candidate, employer
 from sqlalchemy.exc import IntegrityError, DBAPIError
@@ -14,6 +15,11 @@ def all_error(exc, request):
                     headers=[('Content-Type', 'application/json')])
 
 
+def http_error(exc, request):
+    return Response(json.dumps({'db_message': exc.detail}), status_code=exc.code,
+                    headers=[('Content-Type', 'application/json')])
+
+
 def includeme(config):
     config.include(configuration, route_prefix='/config')
     config.include(candidate, route_prefix='/candidates')
@@ -21,3 +27,4 @@ def includeme(config):
 
     config.add_view(context=DBAPIError, view=db_error)
     config.add_view(context=Exception, view=all_error)
+    config.add_view(context=HTTPException, view=http_error)
