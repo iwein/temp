4971659ba674a147c9733efc29c7330089a4de13
A. Matías Quezada <amatiasq@gmail.com>
2014-09-09 16:02:33 +0200

    Fixes #56 - Added filter for employers status

--- scotty/static/components/admin-approve-employers/admin-approve-employers.js
+++ scotty/static/components/admin-approve-employers/admin-approve-employers.js
@@ -4,9 +4,11 @@ define(function(require) {
   var module = require('app-module');
 
   module.controller('AdminApproveEmployers', function($scope, Session) {
+    this.filterEmployers = filterEmployers;
     this.approve = approve;
+    $scope.status = 'applied';
 
-    $scope.employers = [{
+    var employers = [{
       contact_name: 'Marius',
       company_name: 'Intel Corp.',
       status: 'applied',
@@ -15,9 +17,21 @@ define(function(require) {
       company_name: 'Microsoft',
       status: 'applied',
     }, {
+      contact_name: 'Foo',
+      company_name: 'A',
+      status: 'applied',
+    }, {
+      contact_name: 'Bar',
+      company_name: 'B',
+      status: 'status4',
+    }, {
+      contact_name: 'Quz',
+      company_name: 'C',
+      status: 'signedup',
+    }, {
       contact_name: 'Eusebio',
       company_name: 'Cara gamba inc.',
-      status: 'applied',
+      status: 'approved',
     }];
 
     function approve(employer) {
@@ -25,6 +39,14 @@ define(function(require) {
         employer.status = 'approved';
       });
     }
+
+    function filterEmployers(status) {
+      $scope.employers = employers.filter(function(employer) {
+        return employer.status === status;
+      });
+    }
+
+    filterEmployers($scope.status);
   });
 
   return {
