67bf494fe6491c0fb4caf6b5a40cc947485963ed
A. Matías Quezada <amatiasq@gmail.com>
2014-09-09 17:51:39 +0200

    Fixed candidate sign up breadcrumb (broken on workflow change)

--- scotty/static/components/candidate-signup/candidate-login.e2e.js
+++
@@ -1,9 +0,0 @@
-/*globals describe, it, browser, expect */
-'use strict';
-
-describe('Home component', function() {
-  it('should have a title', function() {
-    browser.get('http://localhost/static/public/candidate.html');
-    expect(browser.getTitle()).toEqual('Scotty - Candidate');
-  });
-});
