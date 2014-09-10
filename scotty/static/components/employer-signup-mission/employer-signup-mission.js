986ca4c50b57b2e658749b89c708c4a5ead4f701
A. Matías Quezada <amatiasq@gmail.com>
2014-09-10 20:49:45 +0200

    Fixes #64 - Added mission signup screen with rich text editor Added textAngular as rich text editor Added font-awesome to the project as dependency of textAngular

---
+++ scotty/static/components/employer-signup-mission/employer-signup-mission.js
@@ -0,0 +1,41 @@
+define(function(require) {
+  'use strict';
+  require('textangular');
+  require('session');
+  var module = require('app-module');
+
+  module.controller('SignupMissionCtrl', function($scope, $state, ConfigAPI, Session) {
+    this.submit = submit;
+    $scope.loading = false;
+    $scope.missionDirty = false;
+    $scope.model = {};
+
+    $scope.$watch('model.mission', function(value) {
+      $scope.errorMissionEmpty = !value;
+      $scope.missionDirty = $scope.missionDirty || !!value;
+    });
+
+    function submit() {
+      if (!$scope.errorMissionEmpty)
+        return;
+
+      $scope.loading = true;
+      Session.signup($scope.model).then(function() {
+        $state.go('home');
+      }).catch(function() {
+        $scope.error = true;
+      }).finally(function() {
+        $scope.loading = false;
+      });
+    }
+  });
+
+  return {
+    url: '/mission/',
+    template: require('text!./employer-signup-mission.html'),
+    controller: 'SignupMissionCtrl',
+    controllerAs: 'signupMission',
+  };
+});
+
+
