5d45b5fde03f1007c82da066be2c7bb627bc6248
A. Matías Quezada <amatiasq@gmail.com>
2014-09-12 19:26:59 +0200

    Fixes #73 - Added years of experience to suggestion

--- scotty/static/components/employer-signup-suggest/employer-signup-suggest.js
+++ scotty/static/components/employer-signup-suggest/employer-signup-suggest.js
@@ -12,9 +12,15 @@ define(function(require) {
       return entry.first_name[0] + '. ' + entry.last_name[0] + '.';
     }
 
-    function calcExperience() { // (entry) {
-      // TODO: Calculate experience
-      return Math.round(Math.random() * 15);
+    function calcExperience(experience) {
+      return experience.reduce(function(sum, entry) {
+        var start = new Date(entry.start);
+        var end = new Date(entry.end);
+        var offset = new Date(0);
+        var diff = new Date(end - start);
+        var years = diff.getFullYear() - offset.getFullYear();
+        return sum + years;
+      }, 0);
     }
 
     Session.getSuggestedCandidates().then(function(candidates) {
@@ -22,7 +28,7 @@ define(function(require) {
         return {
           name: anonymize(entry),
           city: ConfigAPI.locationToText(entry.contact_city),
-          yearsOfExperience: calcExperience(entry),
+          yearsOfExperience: calcExperience(entry.work_experience),
           skills: entry.skills.map(function(item) {
             return item.skill;
           })
