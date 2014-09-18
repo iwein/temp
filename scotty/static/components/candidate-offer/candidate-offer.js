d3b9f44699c3f5e4945eeec7d6983d82b46e8e90
A. Matías Quezada <amatiasq@gmail.com>
2014-09-18 19:21:45 +0200

    Fixes #141 - Candidate accept / reject offer

--- scotty/static/components/candidate-offer/candidate-offer.js
+++ scotty/static/components/candidate-offer/candidate-offer.js
@@ -6,7 +6,9 @@ define(function(require) {
   // jshint maxparams:7
   module.controller('OfferCtrl', function($scope, $sce, $state, toaster, ConfigAPI, Permission, Session) {
     this.toggleRejecting = toggleRejecting;
+    $scope.loading = false;
     $scope.ready = false;
+    $scope.model = {};
 
     Permission.requireLogged().then(function() {
       $scope.id = $state.params.id;
@@ -17,10 +19,39 @@ define(function(require) {
 
       return Session.user.getOffer($scope.id);
     }).then(function(offer) {
+      this.accept = acceptOffer;
+      this.reject = rejectOffer;
+      $scope.ready = true;
       $scope.offer = offer;
+      $scope.active = offer.status === 'ACTIVE';
       offer.interview_details = $sce.trustAsHtml(offer.interview_details);
       offer.job_description = $sce.trustAsHtml(offer.job_description);
-      $scope.ready = true;
+
+      function acceptOffer() {
+        $scope.loading = true;
+        Session.user.acceptOffer($scope.id)
+          .then(function() {
+            toaster.success('Offer accepted');
+            $scope.active = false;
+          })
+          .catch(toaster.defaultError)
+          .finally(function() { $scope.loading = false });
+      }
+
+      function rejectOffer() {
+        if (!$scope.model.reason)
+          return;
+
+        $scope.loading = true;
+        Session.user.rejectOffer($scope.id, $scope.model)
+          .then(function() {
+            toaster.success('Offer rejected');
+            $scope.active = false;
+          })
+          .catch(toaster.defaultError)
+          .finally(function() { $scope.loading = false });
+      }
+
     }.bind(this));
 
     function toggleRejecting() {
