f1587ef32ab85a2e9d8dbe2205e7777261acc1b6
A. Matías Quezada <amatiasq@gmail.com>
2014-10-02 15:30:33 +0200

    #231 - Implementing profiles for admin

---
+++ scotty/static/components/admin-candidate-profile/admin-candidate-profile.js
@@ -0,0 +1,33 @@
+define(function(require) {
+  'use strict';
+  require('components/directive-target-positions/directive-target-positions');
+  require('components/directive-experience/directive-experience');
+  require('components/directive-education/directive-education');
+  var module = require('app-module');
+
+  module.controller('CandidateProfileCtrl', function($scope, $state, toaster, Session) {
+    $scope.id = $state.params.id;
+    $scope.ready = false;
+
+    Session.getCandidate($scope.id).then(function(candidate) {
+      $scope.candidate = candidate;
+      return candidate.getData();
+    }).then(function(data) {
+      $scope.ready = true;
+      $scope.cities = data.preferred_location;
+      $scope.languages = data.languages;
+      $scope.skills = data.skills;
+      $scope.user = data;
+    }).catch(function() {
+      toaster.defaultError();
+    });
+  });
+
+
+  return {
+    url: '/candidate/:id',
+    template: require('text!./admin-candidate-profile.html'),
+    controller: 'CandidateProfileCtrl',
+    controllerAs: 'candidateProfile',
+  };
+});
---
+++ scotty/static/components/admin-employer-profile/admin-employer-profile.js
@@ -0,0 +1,59 @@
+define(function(require) {
+  'use strict';
+  require('angular-sanitize');
+  require('components/directive-office/directive-office');
+  var module = require('app-module');
+
+  module.controller('EmployerProfileCtrl', function($scope, $sce, $state, toaster, Permission, Session) {
+    $scope.ready = false;
+
+    Permission.requireLogged().then(function() {
+      this.toogleBookmark = toogleBookmark;
+      $scope.id = $state.params.id;
+
+      getIsBookmarked();
+
+      Session.getEmployer($scope.id).then(function(employer) {
+        $scope.employer = employer;
+        return employer.getData();
+      }).then(function(data) {
+        $scope.ready = true;
+        $scope.data = data;
+        $scope.data.mission_text = $sce.trustAsHtml(data.mission_text);
+      }).catch(function() {
+        toaster.defaultError();
+      });
+
+
+      function getIsBookmarked() {
+        return Session.user.getBookmarks().then(function(results) {
+          $scope.isBookmarked = results.some(function(employer) {
+            return employer.id === $scope.id;
+          });
+          return $scope.isBookmarked;
+        });
+      }
+
+      function toogleBookmark() {
+        ($scope.isBookmarked ?
+          Session.user.deleteBookmark({ id: $scope.id }) :
+          Session.user.addBookmark({ id: $scope.id })
+        ).then(function() {
+          return getIsBookmarked();
+        }).then(function(isBookmarked) {
+          toaster.success(isBookmarked ? 'Employer bookmarked' : 'Bookmark removed');
+        }).catch(function() {
+          toaster.defaultError();
+        });
+      }
+    }.bind(this));
+  });
+
+
+  return {
+    url: '/employer/:id',
+    template: require('text!./admin-employer-profile.html'),
+    controller: 'EmployerProfileCtrl',
+    controllerAs: 'employerProfile',
+  };
+});
