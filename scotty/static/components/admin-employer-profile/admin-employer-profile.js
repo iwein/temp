16c4cdaf2f3132a4264a41d5585ef6c0706051fc
A. Matías Quezada <amatiasq@gmail.com>
2014-10-02 15:56:06 +0200

    Fixes #232 - Employer profile for admin

--- scotty/static/components/admin-employer-profile/admin-employer-profile.js
+++ scotty/static/components/admin-employer-profile/admin-employer-profile.js
@@ -4,49 +4,20 @@ define(function(require) {
   require('components/directive-office/directive-office');
   var module = require('app-module');
 
-  module.controller('EmployerProfileCtrl', function($scope, $sce, $state, toaster, Permission, Session) {
+  module.controller('EmployerProfileCtrl', function($scope, $sce, $state, toaster, Session) {
     $scope.ready = false;
+    $scope.id = $state.params.id;
 
-    Permission.requireLogged().then(function() {
-      this.toogleBookmark = toogleBookmark;
-      $scope.id = $state.params.id;
-
-      getIsBookmarked();
-
-      Session.getEmployer($scope.id).then(function(employer) {
-        $scope.employer = employer;
-        return employer.getData();
-      }).then(function(data) {
-        $scope.ready = true;
-        $scope.data = data;
-        $scope.data.mission_text = $sce.trustAsHtml(data.mission_text);
-      }).catch(function() {
-        toaster.defaultError();
-      });
-
-
-      function getIsBookmarked() {
-        return Session.user.getBookmarks().then(function(results) {
-          $scope.isBookmarked = results.some(function(employer) {
-            return employer.id === $scope.id;
-          });
-          return $scope.isBookmarked;
-        });
-      }
-
-      function toogleBookmark() {
-        ($scope.isBookmarked ?
-          Session.user.deleteBookmark({ id: $scope.id }) :
-          Session.user.addBookmark({ id: $scope.id })
-        ).then(function() {
-          return getIsBookmarked();
-        }).then(function(isBookmarked) {
-          toaster.success(isBookmarked ? 'Employer bookmarked' : 'Bookmark removed');
-        }).catch(function() {
-          toaster.defaultError();
-        });
-      }
-    }.bind(this));
+    Session.getEmployer($scope.id).then(function(employer) {
+      $scope.employer = employer;
+      return employer.getData();
+    }).then(function(data) {
+      $scope.ready = true;
+      $scope.data = data;
+      $scope.data.mission_text = $sce.trustAsHtml(data.mission_text);
+    }).catch(function() {
+      toaster.defaultError();
+    });
   });
 
 
