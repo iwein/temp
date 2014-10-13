9809c5a7659c855df396cc3ec7ce0d50d0c57ee9
A. Matías Quezada <amatiasq@gmail.com>
2014-10-13 15:03:52 +0200

    #274 - added pagination to offer list

--- scotty/static/components/admin-dashboard/admin-dashboard.js
+++ scotty/static/components/admin-dashboard/admin-dashboard.js
@@ -6,22 +6,67 @@ define(function(require) {
 
   module.controller('DashboardCtrl', function($scope, $sce, toaster, Loader, Session) {
     this.refresh = list;
+    $scope.loadMore = loadMore;
+    $scope.filter = filter;
+    $scope.query = {};
+    var show = 15;
+    var page = 1;
+    var all, filtered;
+
+    $scope.statuses = {
+      'ACTIVE': 'Active',
+      'REJECTED': 'Rejected',
+      'ACCEPTED': 'Accept',
+      'INTERVIEW': 'Interview',
+      'CONTRACT_NEGOTIATION': 'Contract negotiation',
+      'CONTRACT_SIGNED': 'Contract signed',
+      'WITHDRAWN': 'Withdrawn',
+      'EXPIRED': 'Expired',
+    };
 
     list().finally(function() {
       Loader.page(false);
     });
 
+    function filter() {
+      var term = $scope.query.company ? $scope.query.company.toUpperCase() : '';
+      filtered = all.filter(function(offer) {
+        if ($scope.query.status && offer.status !== $scope.query.status)
+          return false;
+
+        if (!term)
+          return true;
+
+        var company = offer.data.employer.company_name.toUpperCase();
+        return company.indexOf(term) !== -1;
+      });
+
+      page = 1;
+      render();
+    }
+
+    function loadMore() {
+      page++;
+      render();
+    }
+
+    function render() {
+      $scope.offers = filtered.slice(0, page * show);
+    }
+
     function list() {
       Loader.add('admin-dashboard-offers');
       return Session.getOffers()
-        .then(function(offers) {
-          $scope.offers = offers;
-          offers.map(function(offer) {
+        .then(function(allOffers) {
+          allOffers.forEach(function(offer) {
             offer.setDataParser(function(data) {
               data.interview_details = $sce.trustAsHtml(data.interview_details);
               data.job_description = $sce.trustAsHtml(data.job_description);
             });
           });
+
+          all = allOffers;
+          filter();
         })
         .catch(toaster.defaultError)
         .finally(function() { Loader.remove('admin-dashboard-offers') });
