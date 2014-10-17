e536c7d45bdd765133e8425fc60da9b8d483e668
Martin Peschke <martin@hackandcraft.com>
2014-10-17 15:50:08 +0200

    js styles

--- scotty/static/tools/recency-directive.js
+++ scotty/static/tools/recency-directive.js
@@ -8,25 +8,25 @@ define(function(require) {
             var interval = Math.floor(seconds / 31536000);
 
             if (interval > 1) {
-                return interval + " years ago";
+                return interval + ' years ago';
             }
             interval = Math.floor(seconds / 2592000);
             if (interval > 1) {
-                return interval + " months ago";
+                return interval + ' months ago';
             }
             interval = Math.floor(seconds / 86400);
             if (interval > 1) {
-                return interval + " days ago";
+                return interval + ' days ago';
             }
             interval = Math.floor(seconds / 3600);
             if (interval > 1) {
-                return interval + " hours ago";
+                return interval + ' hours ago';
             }
             interval = Math.floor(seconds / 60);
             if (interval > 1) {
-                return interval + " minutes ago";
+                return interval + ' minutes ago';
             }
-            return Math.floor(seconds) + " seconds ago";
+            return Math.floor(seconds) + ' seconds ago';
         };
     });
 });
