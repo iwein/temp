aed2bde8bcfd73908acf8d281df6c77e9ccb13af
A. Matías Quezada <amatiasq@gmail.com>
2014-11-07 21:28:13 +0100

    Bugfix: signup approve page should not be able to open if candidate is approved

--- scotty/static/components/candidate-signup-approve/candidate-signup-approve.js
+++ scotty/static/components/candidate-signup-approve/candidate-signup-approve.js
@@ -1,7 +1,22 @@
 define(function(require) {
   'use strict';
+  var module = require('app-module');
+
+
+  module.controller('CandidateSignupApproveCtrl', function($scope, $state, Session) {
+    $scope.ready = false;
+    Session.isActivated().then(function(approved) {
+      if (approved)
+        $state.go('profile');
+    }).finally(function() {
+      $scope.ready = true;
+    });
+  });
+
+
   return {
     url: '/approve/',
     template: require('text!./candidate-signup-approve.html'),
+    controller: 'CandidateSignupApproveCtrl',
   };
 });
