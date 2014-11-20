5032925e374c7db846ed6f8f4ebf8191fe886224
A. Matías Quezada <amatiasq@gmail.com>
2014-11-20 19:57:53 +0100

    #441 - Implemented company search at candidate dashboard

--- scotty/static/components/candidate-dashboard/candidate-dashboard.js
+++ scotty/static/components/candidate-dashboard/candidate-dashboard.js
@@ -1,14 +1,26 @@
 define(function(require) {
   'use strict';
   require('components/directive-employer/directive-employer');
+  var _ = require('underscore');
+  var fn = require('tools/fn');
   var module = require('app-module');
 
 
-  module.controller('DashboardCtrl', function($scope, $q, $sce, Loader, Permission, Session) {
-    Loader.page(true);
+  module.controller('DashboardCtrl', function($scope, $q, $sce, toaster, Loader, ConfigAPI, Permission, Session) {
+    $scope.searchLocations = ConfigAPI.locationsText;
+    $scope.searchSkills = ConfigAPI.skills;
+    $scope.setLocation = setLocation;
+    $scope.search = search;
     $scope.toggle = toggle;
     $scope.ready = false;
     $scope.today = new Date();
+    Loader.page(true);
+
+    ConfigAPI.companyTypes().then(function(data) {
+      $scope.companyTypes = data.map(function(type) {
+        return { value: type };
+      });
+    });
 
     Permission.requireActivated().then(function() {
       $scope.ready = true;
@@ -36,6 +48,36 @@ define(function(require) {
 
     function toggle(key) {
       $scope[key] = !$scope[key];
+    }
+
+    function setLocation(text) {
+      $scope.location = ConfigAPI.getLocationFromText(text || $scope.locationText);
+      search();
+    }
+
+    function search() {
+      var tags = $scope.terms && $scope.terms.join();
+      var companyTypes = $scope.companyTypes
+        .filter(fn.get('selected'))
+        .map(fn.get('value'))
+        .join();
+
+      var params = _.extend({},
+        $scope.location,
+        tags && { tags: tags },
+        companyTypes && { company_types: companyTypes }
+      );
+
+      $scope.loading = true;
+      return Session.searchEmployers(params)
+        .then(function(employers) {
+          return $q.all(employers.map(fn.invoke('getData', [])));
+        })
+        .then(function(results) { $scope.employers = results })
+        .catch(toaster.defaultError)
+        .finally(function() {
+          $scope.loading = false;
+        });
     }
   });
 
