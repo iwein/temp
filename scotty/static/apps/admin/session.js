ae87faaa7754489ecc9a36ff8c6f5fc7c51b4c37
A. Matías Quezada <amatiasq@gmail.com>
2014-12-01 14:09:54 +0100

    #516 - Added API key to admin requests

--- scotty/static/apps/admin/session.js
+++ scotty/static/apps/admin/session.js
@@ -5,7 +5,8 @@ define(function(require) {
   var Office = require('apps/common/offer');
   var Employer = require('apps/common/employer');
   var Candidate = require('apps/common/candidate');
-
+  var apiKey = 'apikey=6b23dd93c33e4100ce9332eff5df6e7b01e5a289681cdff';
+  var getParams = '?' + apiKey;
 
   function AdminSession(api, promise) {
     this._api = api;
@@ -16,27 +17,27 @@ define(function(require) {
     constructor: AdminSession,
 
     inviteEmployer: function(model) {
-      return this._api.post('/admin/employers', model);
+      return this._api.post('/admin/employers' + getParams, model);
     },
 
     approveEmployer: function(employer) {
-      return this._api.get('/admin/employers/' + employer.id + '/approve');
+      return this._api.get('/admin/employers/' + employer.id + '/approve' + getParams);
     },
 
     approveCandidate: function(candidate) {
-      return this._api.get('/admin/candidates/' + candidate.id + '/approve');
+      return this._api.get('/admin/candidates/' + candidate.id + '/approve' + getParams);
     },
 
     getEmployersByStatus: function(status) {
-      return this._api.get('/admin/employers/' + status).then(fn.get('data'));
+      return this._api.get('/admin/employers/' + status + getParams).then(fn.get('data'));
     },
 
     getCandidatesByStatus: function(status) {
-      return this._api.get('/candidates?status=' + status).then(fn.get('data'));
+      return this._api.get('/candidates' + getParams + '&status=' + status).then(fn.get('data'));
     },
 
     getOffers: function() {
-      return this._api.get('/admin/offers').then(function(response) {
+      return this._api.get('/admin/offers' + getParams).then(function(response) {
         return response.data.map(function(data) {
           return new Office(this._api, '/admin/offers/' + data.id, data);
         }.bind(this));
@@ -45,40 +46,40 @@ define(function(require) {
 
     getOffer: function(id) {
       var url = '/admin/offers/' + id;
-      return this._api.get(url).then(function(response) {
+      return this._api.get(url + getParams).then(function(response) {
         return new Office(this._api, url, response);
       }.bind(this));
     },
 
     searchEmployers: function(query) {
-      return this._api.get('/admin/search/employers', query);
+      return this._api.get('/admin/search/employers' + getParams, query);
     },
 
     getEmployer: function(id) {
-      return this._api.get('/employers/' + id).then(function(data) {
+      return this._api.get('/employers/' + id + getParams).then(function(data) {
         return new Employer(this._api, data.id, data);
       }.bind(this));
     },
 
 
     searchCandidates: function(query) {
-      return this._api.get('/admin/search/candidates', query);
+      return this._api.get('/admin/search/candidates' + getParams, query);
     },
 
     getCandidate: function(id) {
-      return this._api.get('/candidates/' + id).then(function(data) {
+      return this._api.get('/candidates/' + id + getParams).then(function(data) {
         return new Candidate(this._api, data.id, data);
       }.bind(this));
     },
 
     getInviteCodes: function() {
-      return this._api.get('/admin/invite_codes').then(function(response) {
+      return this._api.get('/admin/invite_codes' + getParams).then(function(response) {
         return response.data;
       });
     },
 
     addInviteCode: function(data) {
-      return this._api.post('/admin/invite_codes', data).then(function(response) {
+      return this._api.post('/admin/invite_codes', data + getParams).then(function(response) {
         if (response.db_message === 'Code already created!')
           throw new Error('DUPLICATED_ENTRY');
         return response;
