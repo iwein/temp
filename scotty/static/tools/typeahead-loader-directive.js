709468df0a89f7c1e9cf78ac07248729bf9ebc4a
A. Matías Quezada <amatiasq@gmail.com>
2015-01-05 19:23:48 +0100

    Fixes #679 - Spinner popup is now aligned

--- scotty/static/tools/typeahead-loader-directive.js
+++ scotty/static/tools/typeahead-loader-directive.js
@@ -6,18 +6,21 @@ define(function(require) {
       priority: 1,
       restrict: 'A',
       compile: function(elem, attr) {
+        var div = document.createElement('div');
         var ul = document.createElement('ul');
         var li = document.createElement('li');
         var spinner = document.createElement('div');
+        div.className = 'typeahead-loader-container';
+        ul.className = 'dropdown-menu';
         spinner.className = 'spinner';
-        ul.className = 'dropdown-menu typeahead-loader-container';
         li.appendChild(spinner);
         ul.appendChild(li);
+        div.appendChild(ul);
 
         var element = elem[0];
         var parent = element.parentNode;
         var sibling = element.nextElementSibling;
-        parent.insertBefore(ul, sibling);
+        parent.insertBefore(div, sibling);
 
         var id = '_autoloader' + Math.round(Math.random() * 10000);
         attr.typeaheadLoading = id;
