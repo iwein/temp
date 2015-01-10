f1ed2b7b94e82f57976abbd2fe274f8b6c8a46da
Martin Peschke <martin@hackandcraft.com>
2015-01-10 20:46:00 +0100

    updated api url

--- scotty/static/config/app-conf-prod.js
+++ scotty/static/config/app-conf-prod.js
@@ -11,7 +11,7 @@
     'amazon_secret_key': 'xWdmpQ8bVSRyDkJ6BLD8i9EUnYWkt8CJ0QjREcD5',
     'amazon_access_key': 'AKIAI72PUNP77JJ4YMFA',
     'amazon_acl': 'public-read',
-    'api_url': 'http://guarded-inlet-9319.herokuapp.com/api/',
+    'api_url': 'http://scotty-prod.herokuapp.com/api/',
     'admin_key': '6b23dd93c33e4100ce9332eff5df6e7b01e5a289681cdff',
     'support_email': 'service@4scotty.com',
     'ga_id': 'UA-22621022-14'
