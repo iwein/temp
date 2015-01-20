abe676f6bb8cb0402cbd27e3067abd9f733b8d01
Martin Peschke <martin@hackandcraft.com>
2015-01-20 21:18:54 +0100

    replace waitress with paste server

--- runapp.py
+++ runapp.py
@@ -1,9 +1,9 @@
 import os
 
 from paste.deploy import loadapp
-from waitress import serve
+from paste.httpserver import serve
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app = loadapp('config:configs/production.ini', relative_to='.')
-    serve(app, host='0.0.0.0', port=port)
+    serve(app, host='0.0.0.0', port=port, use_threadpool=True, threadpool_workers=10, socket_timeout=70)
