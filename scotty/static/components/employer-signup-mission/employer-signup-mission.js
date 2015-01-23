9b4da7ae92d332a4d03fda88fb0e94a39eda4b0b
A. Matías Quezada <amatiasq@gmail.com>
2015-01-23 23:26:37 +0100

    [UNSTABLE] Developing image loader on mission field

--- scotty/static/components/employer-signup-mission/employer-signup-mission.js
+++ scotty/static/components/employer-signup-mission/employer-signup-mission.js
@@ -5,13 +5,90 @@ define(function(require) {
   var _ = require('underscore');
   var module = require('app-module');
 
-  module.controller('SignupMissionCtrl', function($scope, toaster, Loader, Session) {
+  module.directive('onFile', function($parse) {
+    return {
+      restrict: 'A',
+      link: function(scope, elem, attr) {
+        var element = elem[0];
+        var pattern = new RegExp(attr.onFilePattern);
+        var submit = $parse(attr.onFile || attr.handler);
+        var preventDef = 'onFilePreventDefault' in attr;
+
+        function complete(event, locals) {
+          if (preventDef) {
+            event.stopPropagation();
+            event.preventDefault();
+          }
+
+          submit(scope, locals);
+        }
+
+        element.addEventListener('paste', function(event) {
+          var clipboard = event && event.clipboardData;
+          if (!clipboard || !clipboard.getData)
+            return;
+
+          if (clipboard.files.length) {
+            var files = [].filter.call(clipboard.files, function(file) {
+              return pattern.test(file.type);
+            });
+
+            if (files.length)
+              complete(event, { $files: files });
+          }
+
+          /*
+          This just prints the file format icon
+
+          [].some.call(clipboard.types, function(type, index) {
+            var item = clipboard.files[index];
+
+            if (pattern.test(type) || pattern.test(item.type)) {
+              file = item.getAsFile();
+              return true;
+            }
+          });
+
+
+          if (file)
+            submit(scope, { $file: file });
+          */
+        }, true);
+      }
+    };
+  });
+
+  module.controller('SignupMissionCtrl', function($scope, toaster, Amazon, Loader, Session) {
     this.submit = submit;
     $scope.error = false;
     $scope.loading = true;
     $scope.missionDirty = false;
     $scope.model = {};
     Loader.page(true);
+
+
+    // Angular HACK! There is no way to modify textangular content but editing the HTML itself
+    function setMissionText(value) {
+      document.querySelector('text-angular [contenteditable]').innerHTML = value;
+    }
+
+    $scope.onFile = function(files) {
+      // TODO: support multiple files
+      var file = files[0];
+
+      var id = 'IMAGE-' + Math.round(Math.random() * 1000000) + '.' + file.type.split('/')[1];
+      var token = '[uploading file ' + id + '...]';
+      var original = $scope.model.mission_text;
+      var mission = $scope.model.mission_text + token;
+      setMissionText(mission);
+
+      return Amazon.upload(file, 'employer-photos/' + Session.id(), id).then(function(url) {
+        if ($scope.model.mission_text !== original)
+          mission = $scope.model.mission_text;
+        setMissionText(mission.replace(token, '<img width="50px" height="50px" src="' + url + '">'));
+      });
+    };
+
 
     $scope.$watch('model.mission_text', function(value) {
       $scope.errorMissionEmpty = !value;
