27ab3852230e75f6539aaffcbd88efd423922636
A. Matías Quezada <amatiasq@gmail.com>
2015-01-29 02:27:07 +0100

    Fixed email step

--- scotty/static/tools/validate-on-submit-directive.js
+++ scotty/static/tools/validate-on-submit-directive.js
@@ -17,6 +17,7 @@ define(function(require) {
 
             fields.forEach(function(field) {
               field.$dirty = true;
+              if (!field.$name) return;
               var dom = element.querySelector('[name=' + field.$name + ']');
               if (dom) dom.focus();
             });
