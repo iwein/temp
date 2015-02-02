08fb96ea0610a86d6fb485806dfe70f50b9d394f
Martin Peschke <martin@hackandcraft.com>
2015-02-02 20:52:50 +0100

    switch demo back to German

--- scotty/static/apps/common/basic-conf.js
+++ scotty/static/apps/common/basic-conf.js
@@ -55,7 +55,7 @@ define(function(require) {
     });
 
     module.run(function($templateCache, gettextCatalog) {
-      var lang = 'en';
+      var lang = 'de';
       $templateCache.put('footer.html', require('text!../common/footer.html'));
       gettextCatalog.setCurrentLanguage(lang);
       gettextCatalog.debug = true;
