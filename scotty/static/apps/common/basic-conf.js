4f515370b50d6dcf00110e08fd981df9c4c77984
Martin Peschke <martin@hackandcraft.com>
2015-02-04 20:28:44 +0100

    disable xing button, switch back to en

--- scotty/static/apps/common/basic-conf.js
+++ scotty/static/apps/common/basic-conf.js
@@ -54,7 +54,7 @@ define(function(require) {
     });
 
     module.run(function($templateCache, gettextCatalog) {
-      var lang = 'de';
+      var lang = 'en';
       $templateCache.put('footer.html', require('text!../common/footer.html'));
       gettextCatalog.setCurrentLanguage(lang);
       gettextCatalog.debug = true;
