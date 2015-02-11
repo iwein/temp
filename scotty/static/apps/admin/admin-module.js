17564561c2b3da8db969ebac22424c33eb18cc52
A. Matías Quezada <amatiasq@gmail.com>
2015-02-11 17:56:28 +0100

    Injected lightbox component into admin portal

--- scotty/static/apps/admin/admin-module.js
+++ scotty/static/apps/admin/admin-module.js
@@ -3,11 +3,13 @@ define(function(require) {
   require('angular-router');
   require('angular-gettext');
   require('ui.bootstrap');
+  require('angular-bootstrap-lightbox');
   var angular = require('angular');
 
   return angular.module('scotty-admin', [
     'gettext',
     'ui.router',
     'ui.bootstrap',
+    'bootstrapLightbox',
   ]);
 });
