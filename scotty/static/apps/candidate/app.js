8d2ca94561f0fce9aea226f7d770d26f759feb46
A. Matías Quezada <amatiasq@gmail.com>
2015-03-23 18:23:32 +0100

    Removed candidate signup workflow

--- scotty/static/apps/candidate/app.js
+++ scotty/static/apps/candidate/app.js
@@ -37,14 +37,6 @@ define(function(require) {
       .state('offer-list', require('components/candidate-offer-list/candidate-offer-list'))
       .state('offer', require('components/candidate-offer/candidate-offer'))
       .state('signup', require('components/candidate-signup/candidate-signup'))
-      .state('signup.target', require('components/candidate-signup-target/candidate-signup-target'))
-      .state('signup.user', require('components/candidate-signup-user/candidate-signup-user'))
-      .state('signup.experience', require('components/candidate-signup-experience/candidate-signup-experience'))
-      .state('signup.skills', require('components/candidate-signup-skills/candidate-signup-skills'))
-      .state('signup.education', require('components/candidate-signup-education/candidate-signup-education'))
-      .state('signup.languages', require('components/candidate-signup-languages/candidate-signup-languages'))
-      .state('signup.profile', require('components/candidate-signup-profile/candidate-signup-profile'))
-      .state('signup.approve', require('components/candidate-signup-approve/candidate-signup-approve'))
     ;
   });
 
--- scotty/static/components/candidate-signup/candidate-signup.js
+++ scotty/static/components/candidate-signup/candidate-signup.js
@@ -1,48 +1,75 @@
 define(function(require) {
   'use strict';
+  var _ = require('underscore');
   var module = require('app-module');
 
 
-  var validStates = {
-    'start': [ 'signup.user' ],
-    'target_position': [ 'signup.target' ],
-    'work_experience': [ 'signup.experience' ],
-    'education': [ 'signup.education' ],
-    'skills': [ 'signup.skills' ],
-    'languages': [ 'signup.languages' ],
-    'profile': [ 'signup.profile' ],
-    'end': [ 'signup-complete' ]
-  };
-  var order = [
-    'signup.user',
-    'signup.target',
-    'signup.experience',
-    'signup.education',
-    'signup.skills',
-    'signup.languages',
-    'signup.profile',
-    'signup-complete',
-  ];
+  module.controller('CandidateSignupCtrl', function($scope, $q, $state, toaster, i18n, Loader, ConfigAPI, Session) {
+    _.extend($scope, {
+      onEmailChange: onEmailChange,
+      submit: submit,
+      loading: false,
+      model: {},
+      errorEmailAlreadyRegistered: false,
+    });
+
+    return onLoad();
+
+    function onLoad() {
+      // HACK: we have to do this in order to have a live translation & register the token
+      // It's critical that `message` and `.gettext` argument have EXACTLY the same content.
+      var message = '<h2>Sign up as IT professional and get hired!</h2>' +
+          'If you are an employer, click <a href="/employer/signup"><b>here</b></a>!';
+      i18n.gettext('<h2>Sign up as IT professional and get hired!</h2>' +
+          'If you are an employer, click <a href="/employer/signup"><b>here</b></a>!');
+
+      toaster.show('alert banner-message', '<translate>' + message + '</translate>', {
+        html: true,
+        untilStateChange: true
+      });
+    }
 
 
-  module.controller('CandidateSignupCtrl', function($scope, $state, Session) {
-    this.target = { company_types: [] };
-    this.cities = [];
+    function onEmailChange() {
+      $scope.errorEmailAlreadyRegistered = false;
+    }
 
-    // This will fire connectors checking
-    Session.getConnectors().getConnected();
+    function submit() {
+      if (!$scope.formSignupUser.$valid)return;
+      $scope.loading = true;
+      $scope.model.locale = i18n.getCurrent();
+      Loader.add('signup-user-saving');
 
-    // Create and invoke controller
-    require('tools/signup-controller')('candidate', order, validStates)
-      .call(this, $scope, $state, Session);
+      return Session.signup($scope.model).then(function() {
+        return $state.go('signup-complete');
+      }).catch(function(request) {
+        if (request.status === 409) {
+          $scope.errorEmailAlreadyRegistered = true;
+          return;
+        }
+
+        if (request.status === 400 && request.data.errors) {
+          if(request.data.errors.invite_code === 'INVALID CHOICE')
+            toaster.error(i18n.gettext('Unknown invite code'));
+          else if(request.data.errors.email)
+              $scope.formSignupUser.email.$setValidity('email', false);
+          return;
+        }
+
+        throw request;
+      }).catch(function() {
+        toaster.defaultError();
+      }).finally(function() {
+        $scope.loading = false;
+        Loader.remove('signup-user-saving');
+      });
+    }
   });
-
 
 
   return {
     url: '/signup',
     template: require('text!./candidate-signup.html'),
     controller: 'CandidateSignupCtrl',
-    controllerAs: 'signup'
   };
 });
