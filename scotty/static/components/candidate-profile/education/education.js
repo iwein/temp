82c664f80a88e84b72c8567831d7c5f2eca40dbf
A. Matías Quezada <amatiasq@gmail.com>
2015-03-26 13:31:55 +0100

    Added missing files

---
+++ scotty/static/components/candidate-profile/education/education.js
@@ -0,0 +1,56 @@
+define(function(require) {
+  'use strict';
+  require('components/directive-education-form/directive-education-form');
+  var _ = require('underscore');
+  var fn = require('tools/fn');
+  var module = require('app-module');
+
+
+  module.directive('hcCandidateEducation', function() {
+    return {
+      template: require('text!./education.html'),
+      scope: { model: '=' },
+      link: function(scope, elem, attr) {
+        // For some reason JSHint complains if I move this function to the end
+        function close() {
+          scope.editing = -1;
+          return profile.closeForm('education');
+        }
+
+
+        var profile = scope.profile = scope.$parent.profile;
+        _.extend(scope, {
+          formContainer: {},
+          editable: 'hcEditable' in attr,
+          close: close,
+          edit: edit,
+          save: save,
+          add: add,
+        });
+
+        scope.model.getHighestDegree().then(fn.setTo('highestDegree', scope));
+        scope.$watch('model.$revision', function() {
+          scope.data = scope.model.getEducationCached();
+        });
+
+
+        function add() {
+          scope.formContainer.form.reset();
+          scope.editing = -2;
+          return profile.openForm('education');
+        }
+        function edit(model, index) {
+          scope.editing = index;
+          return profile.openForm('education');
+        }
+        function save(model, form) {
+          scope.loading = true;
+          return form.save()
+            .then(scope.model.getEducation.bind(scope.model))
+            .then(close)
+            .finally(function() { scope.loading = false });
+        }
+      }
+    };
+  });
+});
---
+++ scotty/static/components/candidate-profile/experience/experience.js
@@ -0,0 +1,83 @@
+define(function(require) {
+  'use strict';
+  require('components/directive-experience-form/directive-experience-form');
+  var _ = require('underscore');
+  var module = require('app-module');
+
+
+  module.directive('hcCandidateExperience', function() {
+    return {
+      template: require('text!./experience.html'),
+      scope: { model: '=' },
+      link: function(scope, elem, attr) {
+        // For some reason JSHint complains if I move this function to the end
+        function close() {
+          scope.editing = -1;
+          return profile.closeForm('experience');
+        }
+
+
+        var profile = scope.profile = scope.$parent.profile;
+        _.extend(scope, {
+          formContainer: {},
+          editable: 'hcEditable' in attr,
+          close: close,
+          edit: edit,
+          save: save,
+          add: add,
+        });
+
+        scope.$watch('model.$revision', function() {
+          scope.data = update(scope.model.getExperienceCached());
+        });
+
+
+        function add() {
+          scope.formContainer.form.reset();
+          scope.editing = -2;
+          return profile.openForm('experience');
+        }
+        function edit(model, index) {
+          scope.editing = index;
+          return profile.openForm('experience');
+        }
+        function save(model, form) {
+          scope.loading = true;
+          return form.save()
+            .then(scope.model.getExperience.bind(scope.model))
+            .then(close)
+            .finally(function() { scope.loading = false });
+        }
+
+        function update(list) {
+          if (!list) return;
+
+          var total = 0;
+          var timeline = list.map(function(entry) {
+            var start = Date.parse(entry.start);
+            var end = entry.end ? Date.parse(entry.end) : Date.now();
+            var duration = end - start;
+            total += duration;
+            return {
+              start: start,
+              duration: duration,
+              role: entry.role
+            };
+          }).filter(function(entry) {
+            return entry.duration !== 0;
+          });
+
+          timeline.forEach(function(entry) {
+            entry.percent = 100 / total * entry.duration;
+          });
+          scope.totalWorkExperience = total;
+          scope.timeline = timeline.sort(function(a, b) {
+            return a.start - b.start;
+          });
+
+          return list;
+        }
+      }
+    };
+  });
+});
