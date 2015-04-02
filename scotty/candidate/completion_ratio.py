e39095e22f4f5cc038b9604f4077d1d7e4a838de
Martin Peschke <martin@hackandcraft.com>
2015-04-02 16:54:37 +0200

    add completion percentage for work experience

--- scotty/candidate/completion_ratio.py
+++ scotty/candidate/completion_ratio.py
@@ -21,8 +21,32 @@ def completion_preferred_location(candidate):
 
 
 def completion_work_experience(candidate):
-    return 0
+    wxp_list = candidate.work_experience
+    if not wxp_list:
+        return 0
+    ratio = 0  # when there is something
 
+    #everything has role and company
+
+    company = True
+    title = True
+    summary = True
+    startend = True
+    for wxp in wxp_list:
+        company = company and bool(wxp.company_id)
+        title = title and bool(wxp.role_id)
+        summary = summary and bool(wxp.summary)
+        startend = startend and bool(wxp.start)
+
+    if company:
+        ratio += .4
+    if title:
+        ratio += .2
+    if summary:
+        ratio += .2
+    if startend:
+        ratio += .2
+    return ratio
 
 def completion_education(candidate):
     return 0
@@ -40,10 +64,25 @@ def completion_extended_profile(candidate):
     return 0
 
 
+COMPLETION_CALCULATOR = [
+    {'percentage': .10, 'determinant': completion_base_profile},
+    {'percentage': .10, 'determinant': completion_target_position},
+    {'percentage': .05, 'determinant': completion_preferred_location},
+    {'percentage': .15, 'determinant': completion_work_experience},
+    {'percentage': .15, 'determinant': completion_education},
+    {'percentage': .15, 'determinant': completion_skills},
+    {'percentage': .15, 'determinant': completion_languages},
+    {'percentage': .15, 'determinant': completion_extended_profile}
+]
+
+assert sum([x['percentage'] for x in COMPLETION_CALCULATOR]) == 1
+
+
 def completion_ratio(candidate):
     """
-    >>> from scotty.candidate.models import Candidate, TargetPosition
+    >>> from scotty.candidate.models import Candidate, TargetPosition, WorkExperience
     >>> from scotty.configuration.models import Skill
+    >>> from datetime import datetime
     >>> c = Candidate(email = 'm@m.com')
     >>> completion_ratio(c)
     0.1
@@ -60,23 +99,22 @@ def completion_ratio(candidate):
     >>> completion_ratio(c)
     0.2
 
+    >>> c.work_experience = [WorkExperience(company_id=1)]
+    >>> completion_ratio(c)
+    0.26
+
+    >>> c.work_experience[0].role_id = 1
+    >>> c.work_experience[0].summary = 'summary'
+    >>> c.work_experience[0].start = datetime.now()
+    >>> completion_ratio(c)
+    0.35
+
     :param candidate:
     :return:
     """
 
-    completion = [
-        {'percentage': .10, 'determinant': completion_base_profile},
-        {'percentage': .10, 'determinant': completion_target_position},
-        {'percentage': .05, 'determinant': completion_preferred_location},
-        {'percentage': .15, 'determinant': completion_work_experience},
-        {'percentage': .15, 'determinant': completion_education},
-        {'percentage': .15, 'determinant': completion_skills},
-        {'percentage': .15, 'determinant': completion_languages},
-        {'percentage': .15, 'determinant': completion_extended_profile},
-    ]
-
     ratio = 0
-    for el in completion:
+    for el in COMPLETION_CALCULATOR:
         ratio += el['percentage'] * el['determinant'](candidate)
 
     return ratio
