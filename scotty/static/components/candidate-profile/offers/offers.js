32e26de78d814015e5ab1d2ac7c42e07aad8001a
A. Matías Quezada <amatiasq@gmail.com>
2015-04-16 11:36:28 +0200

    Restored offer box on candidate profile

--- scotty/static/components/candidate-profile/offers/offers.js
+++ scotty/static/components/candidate-profile/offers/offers.js
@@ -7,14 +7,22 @@ define(function(require) {
     return {
       template: require('text!./offers.html'),
       scope: { model: '=' },
-      link: function() {
-        // TODO
+      link: function(scope, elem, attr) {
+        scope.showViewAll = 'hcViewAll' in attr;
 
-        /*
-        $scope.offers = data[2]
-          .sort(function(a, b) { return b.data.annual_salary - a.data.annual_salary })
-          .slice(0, 3);
-        */
+        scope.$watch('model.$revision', function() {
+          var offers = scope.model.getOffersCached() || [];
+          scope.offers = offers
+            .sort(function(a, b) { return b.data.annual_salary - a.data.annual_salary })
+            .slice(0, 3);
+
+          elem.parent()[isEmpty() ? 'addClass' : 'removeClass']('empty');
+        });
+
+        function isEmpty() {
+          return !(scope.offers && scope.offers.length);
+        }
+
       }
     };
   });
