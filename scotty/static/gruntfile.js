c6d0f84eebec7a0e5d909f8049a33a08ccba6805
Petran <calafatidis@gmail.com>
2015-06-06 10:03:24 -0400

    enviroment setup

--- scotty/static/gruntfile.js
+++ scotty/static/gruntfile.js
@@ -35,7 +35,43 @@ module.exports = function(grunt) {
     karma: grunt.file.readJSON('config/grunt-karma.json'),
     protractor: grunt.file.readJSON('config/grunt-protractor.json'),
     protractor_webdriver: grunt.file.readJSON('config/grunt-protractor_webdriver.json'),
-    
+    //TODO REMOVE THAT IN A SEPERATE FILE
+    'http-server': {
+ 
+        'dev': {
+ 
+            // the server root directory 
+            root: apps,
+ 
+            // the server port 
+            // can also be written as a function, e.g. 
+            // port: function() { return 8282; } 
+            port: 8282,
+            
+ 
+            // the host ip address 
+            // If specified to, for example, "127.0.0.1" the server will  
+            // only be available on that ip. 
+            // Specify "0.0.0.0" to be available everywhere 
+            host: "127.0.0.1",
+ 
+            cache: 10,
+            showDir : true,
+            autoIndex: true,
+ 
+            // server default file extension 
+            ext: "html",
+ 
+            // run in parallel with other tasks 
+            runInBackground: true,
+ 
+            // specify a logger function. By default the requests are 
+            // sent to stdout. 
+            logFn: function(req, res, error) { }
+ 
+        }
+ 
+    },
   });
 
   grunt.loadNpmTasks('grunt-serve');
